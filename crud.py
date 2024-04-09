2c7cf4c4b1d94cce375114993d7602b888e90b80
Eugene Protsenko <guitaaarpro@gmail.com>
2024-04-09 19:02:49 +0200

    Solution

--- crud.py
+++ crud.py
@@ -1,22 +1,25 @@
+from typing import List, Optional
 from sqlalchemy.orm import Session
 
 from db import models
 import schemas
 
 
-def get_all_authors(db: Session, skip: int = 0, limit: int = 10):
+def get_all_authors(
+    db: Session, skip: int = 0, limit: int = 10
+) -> List[models.DBAuthor]:
     return db.query(models.DBAuthor).offset(skip).limit(limit).all()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(db: Session, name: str) -> Optional[models.DBAuthor]:
     return db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
 
 
-def get_author_by_id(db: Session, author_id: int):
+def get_author_by_id(db: Session, author_id: int) -> Optional[models.DBAuthor]:
     return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.DBAuthor:
     db_author = models.DBAuthor(name=author.name, bio=author.bio)
     db.add(db_author)
     db.commit()
@@ -24,13 +27,13 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_all_books(db: Session, skip: int = 0, limit: int = 10):
+def get_all_books(db: Session, skip: int = 0, limit: int = 10) -> List[models.DBBook]:
     return db.query(models.DBBook).offset(skip).limit(limit).all()
 
 
 def get_book_by_author_id(
     db: Session, author_id: int | None = None, skip: int = 0, limit: int = 10
-):
+) -> List[models.DBBook]:
     queryset = db.query(models.DBBook)
 
     if author_id is not None:
